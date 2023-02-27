ef1f93cf6a4db216fb7fc214281858f679205767
Larissa Ramos <devlarissasr@gmail.com>
2023-02-27 16:27:11 -0300

    feat: sidebar

---
+++ dashboard/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '@/components/Sidebar'
+
+const home = () => {
+  return (
+    <div className='w-full md:h-screen'>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default home
